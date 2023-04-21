a65adcc22eccc70539fb244722e83976ca655251
Zach Keskinen <Zachkeskinen@gmail.com>
2023-04-21 10:45:24 -0600

    adding sd and coherence to interval measuremetns

--- src/processing/interval_add_sd.py
+++ src/processing/interval_add_sd.py
@@ -50,10 +50,39 @@ for fp in interval_dir.glob('*.csv'):
             gdf.loc[i, 'int_sd_delta'] = np.random.random()
             gdf.loc[i, 'unw_sd_delta'] = np.random.random()
             ds = xr.load_dataset(sub_set[0].fp)
+
             tol = 0.00090009
-            gdf.loc[i, 'int_sd_delta'] = ds['sd_delta_int'].sel(x = slice(r.geometry.x - tol, r.geometry.x + tol), y = slice(r.geometry.y + tol, r.geometry.y - tol)).mean()
+            for band in ['VV','VH','HV','HH']:
+                gdf.loc[i, f'100_int_sd_delta_{band}'] = ds['sd_delta_int'].sel(band = band).sel(x = slice(r.geometry.x - tol, r.geometry.x + tol), y = slice(r.geometry.y + tol, r.geometry.y - tol)).mean()
+                if 'sd_delta_unw' in ds.data_vars:
+                    gdf.loc[i, f'100_unw_sd_delta_{band}'] = ds['sd_delta_unw'].sel(band = band).sel(x = slice(r.geometry.x - tol, r.geometry.x + tol), y = slice(r.geometry.y + tol, r.geometry.y - tol)).mean()
+                
+                tol = 0.00090009* 3
+                gdf.loc[i, f'300_int_sd_delta_{band}'] = ds['sd_delta_int'].sel(band = band).sel(x = slice(r.geometry.x - tol, r.geometry.x + tol), y = slice(r.geometry.y + tol, r.geometry.y - tol)).mean()
+                if 'sd_delta_unw' in ds.data_vars:
+                    gdf.loc[i, f'300_unw_sd_delta_{band}'] = ds['sd_delta_unw'].sel(band = band).sel(x = slice(r.geometry.x - tol, r.geometry.x + tol), y = slice(r.geometry.y + tol, r.geometry.y - tol)).mean()
+                
+                tol = 0.00090009* 5
+                gdf.loc[i, f'500_int_sd_delta_{band}'] = ds['sd_delta_int'].sel(band = band).sel(x = slice(r.geometry.x - tol, r.geometry.x + tol), y = slice(r.geometry.y + tol, r.geometry.y - tol)).mean()
+                if 'sd_delta_unw' in ds.data_vars:
+                    gdf.loc[i, f'500_unw_sd_delta_{band}'] = ds['sd_delta_unw'].sel(band = band).sel(x = slice(r.geometry.x - tol, r.geometry.x + tol), y = slice(r.geometry.y + tol, r.geometry.y - tol)).mean()
+                
+                gdf.loc[i, f'nearest_int_sd_delta_{band}'] = ds['sd_delta_int'].sel(band = band).sel(x = r.geometry.x, y = r.geometry.y, method = 'nearest')
+                if 'sd_delta_unw' in ds.data_vars:
+                    gdf.loc[i, f'nearest_unw_sd_delta_{band}'] = ds['sd_delta_unw'].sel(band = band).sel(x = r.geometry.x, y = r.geometry.y, method = 'nearest')
+            
+            tol = 0.00090009* 3
+            band = 'VV'
+            sub = ds.sel(band = band).sel(x = slice(r.geometry.x - tol, r.geometry.x + tol), y = slice(r.geometry.y + tol, r.geometry.y - tol))
+            gdf.loc[i, '300_coh'] = sub['cor'].mean()
+            gdf.loc[i, f'cor_0.5_300_int_sd_delta_{band}'] = sub.where(sub['cor'] > 0.5)['sd_delta_int'].mean()
             if 'sd_delta_unw' in ds.data_vars:
-                gdf.loc[i, 'unw_sd_delta'] = ds['sd_delta_unw'].sel(x = slice(r.geometry.x - tol, r.geometry.x + tol), y = slice(r.geometry.y + tol, r.geometry.y - tol)).mean()
+                gdf.loc[i, f'cor_0.5_300_unw_sd_delta_{band}'] = sub.where(sub['cor'] > 0.5)['sd_delta_unw'].mean()
+
+            gdf.loc[i, f'cor_0.6_300_int_sd_delta_{band}'] = sub.where(sub['cor'] > 0.6)['sd_delta_int'].mean()
+            if 'sd_delta_unw' in ds.data_vars:
+                gdf.loc[i, f'cor_0.6_300_unw_sd_delta_{band}'] = sub.where(sub['cor'] > 0.6)['sd_delta_unw'].mean()
+    
     res.append(gdf)
 res = pd.concat(res, ignore_index= True)
-res.to_csv('/bsuhome/zacharykeskinen/uavsar-validation/data/interval/interval_sd.csv')
+res.to_csv('/bsuhome/zacharykeskinen/uavsar-validation/data/interval/interval_sd_v3.csv')
