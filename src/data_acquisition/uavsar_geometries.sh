4742f719bdf6295cb13070a80a682d42cbf01702
Zach Keskinen <Zachkeskinen@gmail.com>
2023-05-03 09:36:46 -0600

    rerunning analysis

---
+++ src/data_acquisition/uavsar_geometries.sh
@@ -0,0 +1,12 @@
+cd ~
+cd scratch/data/uavsar/
+mkdir uavsar_geometry
+cd uavsar_geometry
+
+wget https://downloaduav2.jpl.nasa.gov/Release30/lowman_23205_01/lowman_23205_01_BC.dop
+wget https://downloaduav2.jpl.nasa.gov/Release30/lowman_23205_01/lowman_23205_01_BC_s1_2x8.llh
+wget https://downloaduav2.jpl.nasa.gov/Release30/lowman_23205_01/lowman_23205_01_BC_s1_2x8.lkv
+
+wget https://downloaduav2.jpl.nasa.gov/Release30/lowman_05208_01/lowman_05208_01_BU.dop
+wget https://downloaduav2.jpl.nasa.gov/Release30/lowman_05208_01/lowman_05208_01_BU_s1_2x8.llh
+wget https://downloaduav2.jpl.nasa.gov/Release30/lowman_05208_01/lowman_05208_01_BU_s1_2x8.lkv
