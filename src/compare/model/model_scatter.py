f7080d83d1e6b537f230d1e60363a2ae40e7b700
Zach Keskinen <Zachkeskinen@gmail.com>
2023-06-30 12:03:04 -0600

    better scatters

--- src/compare/model/model_scatter.py
+++ src/compare/model/model_scatter.py
@@ -87,7 +87,7 @@ for i, (ax, [xs, ys]) in enumerate(zip(axes, [[xs, ys], [xs_dry, ys_dry], [xs_be
         ax.set_xlim(-0.1, 0.075)
         ax.set_ylim(-0.1, 0.075)
     else:
-        ax.hist2d(xs, ys, bins = 30, norm=mpl.colors.LogNorm(), cmap=mpl.cm.inferno, range = [[-0.03, 0.075],[-0.03, 0.075]])
+        ax.hist2d(xs, ys, bins = 100, norm=mpl.colors.LogNorm(), cmap=mpl.cm.inferno, range = [[-0.03, 0.075],[-0.03, 0.075]])
         ax.set_xlim(-0.1, 0.075)
         ax.set_ylim(-0.1, 0.075)
     ax.plot([-0.1,0.2], [-0.1,0.2], color = 'blue', linestyle = 'dashed')
